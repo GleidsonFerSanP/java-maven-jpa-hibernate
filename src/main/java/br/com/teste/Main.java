fb7a9d525677f29c129a0dc4501e90ae7fd2f60f
Gleidson FerSanP <gleidsonfersanp@MacBook-Pro-de-Gleidson-FerSanP.local>
2018-06-02 19:08:32 -0300

    first commit

---
+++ src/main/java/br/com/teste/Main.java
@@ -0,0 +1,24 @@
+package br.com.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.teste.model.Cliente;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("teste");
+		EntityManager em = emf.createEntityManager();
+
+		Cliente cliente = new Cliente();
+		em.getTransaction().begin();
+		cliente.setNome("Muller Antunes");
+		em.getTransaction().commit();
+		
+		System.out.println("Cliente alterado com sucesso!");
+	}
+
+}
